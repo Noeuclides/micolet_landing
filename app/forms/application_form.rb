a9857a9d6a15c1c61c9480d39d2365282607bbc6
Nicolás Martínez <nicolas.martinez@symplifica.com>
2023-05-15 14:41:55 -0500

    remove unneecsary validate_attributes method on ApplicationForm

--- app/forms/application_form.rb
+++ app/forms/application_form.rb
@@ -1,9 +1,2 @@
 class ApplicationForm
-  def validate_attributes(*attributes)
-    attributes.each do |attribute|
-      self.class.validators_on(attribute).each do |validator|
-        validator.validate_each(self, attribute, send(attribute))
-      end
-    end
-  end
 end
